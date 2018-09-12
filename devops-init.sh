0719d3908d609b72e73e93fab5ccfa7a985cd266
Sven Wagener <sven@awesome.ug>
2018-09-12 14:52:23 +0200

    Fixed wrong filename

--- devops-init.sh
+++ devops-init.sh
@@ -4,6 +4,11 @@ bin_dir="$(linked_file=$(readlink ${0}) && cd $(cd $(dirname ${0}) && echo $(pwd
 includes_dir="${bin_dir}/includes"
 
 ln "${bin_dir}/wp.sh" "$(pwd)/wp.sh"
-ln "${bin_dir}/wp-user-create_admin.sh" "$(pwd)/wp-user-create_admin.sh"
+ln "${bin_dir}/wp-user-create-admin.sh" "$(pwd)/wp-user-create-admin.sh"
 
 ln "${bin_dir}/import-db.sh" "$(pwd)/import-db.sh"
+
+project_name=$(basname $(pwd))
+
+ln "${bin_dir}/webserver-start.sh" "${HOME}/desktop/${project_name}-start"
+ln "${bin_dir}/webserver-stop.sh" "${HOME}/desktop/${project_name}-stop"
