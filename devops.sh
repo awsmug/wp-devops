499d21088b704def4427ee5e674e5b5f85eedd93
Sven Wagener <sven@awesome.ug>
2018-10-12 12:01:55 +0200

    Removed not used code

--- devops.sh
+++ devops.sh
@@ -8,18 +8,3 @@ ln -s "${bin_dir}/server.sh" "$(pwd)/server.sh"
 
 rm "$(pwd)/wp.sh"
 ln -s "${bin_dir}/wp.sh" "$(pwd)/wp.sh"
-
-#rm "$(pwd)/wp-user-create-admin.sh"
-#ln -s "${bin_dir}/wp-user-create-admin.sh" "$(pwd)/wp-user-create-admin.sh"
-
-#rm "$(pwd)/sync.sh"
-#ln -s "${bin_dir}/sync.sh" "$(pwd)/sync.sh"
-
-#project_name=$(basename $(pwd))
-
-#rm "${HOME}/desktop/${project_name}-start"
-#ln -s "${bin_dir}/webserver-start.sh" "${HOME}/desktop/${project_name}-start"
-
-#rm "${HOME}/desktop/${project_name}-stop"
-#ln -s "${bin_dir}/webserver-stop.sh" "${HOME}/desktop/${project_name}-stop"
-#ln -s "${bin_dir}/webserver-stop.sh" "${HOME}/desktop/${project_name}-stop"
